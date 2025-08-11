3226b5d83a7de6e7a198585a9db32960558b8bd3
xirzo <easttem@gmail.com>
2025-08-11 19:30:33 +0300

    refactor: get_config_directory

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -6,47 +6,46 @@ namespace pc {
 
 constexpr std::string APP_NAME = "blueplate";
 
-std::optional<fs::path> get_templates_directory() {
-    fs::path templates_path;
+std::optional<fs::path> get_config_path() {
+    fs::path config_path;
 
 #if defined(_WIN32)
     if (const char *appdata = std::getenv("APPDATA")) {
-        templates_path = fs::path(appdata) / APP_NAME / "templates";
+        config_path = fs::path(appdata) / APP_NAME;
     }
 #elif defined(__APPLE__)
     if (const char *home = std::getenv("HOME")) {
-        templates_path = fs::path(home) / "Library" / "Application Support"
-                       / APP_NAME / "templates";
+        config_path =
+            fs::path(home) / "Library" / "Application Support" / APP_NAME;
     }
 #else
     if (const char *home = std::getenv("HOME")) {
-        templates_path = fs::path(home) / ".config" / APP_NAME / "templates";
+        config_path = fs::path(home) / ".config" / APP_NAME;
     }
 
-    if (!fs::exists(templates_path)) {
+    if (!fs::exists(config_path)) {
         if (const char *xdg_config = std::getenv("XDG_CONFIG_HOME")) {
-            templates_path = fs::path(xdg_config) / APP_NAME / "templates";
+            config_path = fs::path(xdg_config) / APP_NAME;
         }
     }
 #endif
 
     std::vector<fs::path> fallbacks = {
-        fs::current_path() / "templates",
+        fs::current_path(),
 
 #if defined(_WIN32)
         fs::path(
             std::getenv("ProgramFiles") ? std::getenv("ProgramFiles")
                                         : "C:\\Program Files"
-        ) / APP_NAME
-            / "templates",
+        ) / APP_NAME,
 #else
-        "/usr/local/share/" + APP_NAME + "/templates",
-        "/usr/share/" + APP_NAME + "/templates",
+        "/usr/local/share/" + APP_NAME,
+        "/usr/share/" + APP_NAME,
 #endif
     };
 
-    if (fs::exists(templates_path)) {
-        return templates_path;
+    if (fs::exists(config_path)) {
+        return config_path;
     }
 
     for (const auto &path : fallbacks) {
@@ -57,4 +56,19 @@ std::optional<fs::path> get_templates_directory() {
 
     return std::nullopt;
 }
+
+std::optional<fs::path> get_templates_directory() {
+    std::optional<fs::path> config_path = get_config_path();
+
+    if (!config_path) {
+        return std::nullopt;
+    }
+
+    return *config_path / "templates";
+}
+
+std::expected<void, std::string> create_sample_config() {
+    return {};
+}
+
 }  // namespace pc
--- src/configuration.h
+++ src/configuration.h
@@ -1,6 +1,7 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
+#include <expected>
 #include <optional>
 #include <filesystem>
 
@@ -9,6 +10,11 @@ namespace fs = std::filesystem;
 namespace pc {
 
 std::optional<fs::path> get_templates_directory();
-}
+
+std::expected<void, std::string> create_sample_config(
+    const fs::path &template_path
+);
+
+}  // namespace pc
 
 #endif  // CONFIGURATION_H
