580f9898ffbea5503315ee077d6ab6e0a40c2be9
xirzo <easttem@gmail.com>
2025-08-11 18:51:00 +0300

    feat: non-interactive template choosing

--- src/app.cpp
+++ src/app.cpp
@@ -12,11 +12,18 @@ namespace pc {
 
 static CLI::App    s_App{ "CLI tool for fast creating of new projects" };
 static std::string s_ProjectName{};
+static std::string s_ChosenTemplate{};
 
 int run_app(int argc, char **argv) {
     argv = s_App.ensure_utf8(argv);
 
     CLI::App *create = s_App.add_subcommand("create", "Create a new project");
+
+    create->add_option(
+        "-t,--template",
+        s_ChosenTemplate,
+        "Template name (allows non-interactive use)"
+    );
 
     create->add_option("name", s_ProjectName, "Project name")->required();
 
@@ -40,15 +47,16 @@ int run_app(int argc, char **argv) {
                       << std::endl;
         }
 
-        std::string chosen_template =
-            run_selector(names, "Select a project template");
+        if (s_ChosenTemplate.empty()) {
+            s_ChosenTemplate = run_selector(names, "Select a project template");
+        }
 
-        if (chosen_template.empty()) {
+        if (s_ChosenTemplate.empty()) {
             return;
         }
 
         std::expected<void, std::string> create_result =
-            create_project(s_ProjectName, chosen_template);
+            create_project(s_ProjectName, s_ChosenTemplate);
 
         if (!create_result) {
             std::cerr << create_result.error() << std::endl;
