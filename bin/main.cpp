f01971d6ce48d4ea2f015501080eb9069cf6afc3
xirzo <easttem@gmail.com>
2025-08-11 16:29:13 +0300

    feat: connect ui to config directory

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,5 +1,11 @@
 #include "app.h"
 
+/*
+ * TODO: add a way to insert project_name or other variables inside the text of
+ * templates for instance: ${pc_project_name} should be replace in any place
+ * with project_name variable content
+ */
+
 int main(int argc, char **argv) {
     return pc::run_app(argc, argv);
 }
--- src/app.cpp
+++ src/app.cpp
@@ -1,8 +1,12 @@
 #include "app.h"
+#include <filesystem>
+#include <ranges>
 
 #include "CLI11.hpp"
 #include "project_creator.h"
 #include "ui.h"
+
+namespace fs = std::filesystem;
 
 namespace pc {
 
@@ -17,12 +21,27 @@ int run_app(int argc, char **argv) {
     create->add_option("name", s_ProjectName, "Project name")->required();
 
     create->callback([&]() {
-        std::string chosen_template;
+        std::optional<fs::path> template_path = get_templates_directory();
 
-        chosen_template = run_selector(
-            { "C++ Meson", "C Meson", "C++ CMake", "C CMake", "test-project" },
-            "Select a project template"
-        );
+        if (!template_path) {
+            std::cerr << "Could not find template directory" << std::endl;
+            return;
+        }
+
+        std::vector<std::string> names =
+            fs::directory_iterator(*template_path)
+            | std::views::transform([](fs::directory_entry const &entry) {
+                  return entry.path().filename();
+              })
+            | std::ranges::to<std::vector<std::string>>();
+
+        if (names.empty()) {
+            std::cerr << "There are no templates in config directory"
+                      << std::endl;
+        }
+
+        std::string chosen_template =
+            run_selector(names, "Select a project template");
 
         if (chosen_template.empty()) {
             return;
@@ -33,6 +52,7 @@ int run_app(int argc, char **argv) {
 
         if (!create_result) {
             std::cerr << create_result.error() << std::endl;
+            return;
         }
     });
 
