46b2f94d41f632d27bfbf4fb437414dd085d62ab
xirzo <easttem@gmail.com>
2025-08-11 18:40:32 +0300

    fix: creating project when cancelled

--- src/ui.cpp
+++ src/ui.cpp
@@ -10,11 +10,11 @@ std::string run_selector(
     const std::vector<std::string> &options,
     const std::string              &description
 ) {
-    int  selected = 0;
-    bool cancelled = false;
-    bool confirmed = false;
+    int  selected_id = 0;
+    bool has_cancelled = false;
+    bool has_confirmed = false;
 
-    auto component = Menu(&options, &selected);
+    auto component = Menu(&options, &selected_id);
 
     auto ui = Renderer(component, [&] {
         return vbox(
@@ -33,12 +33,12 @@ std::string run_selector(
 
     auto with_keys = CatchEvent(ui, [&](Event event) {
         if (event == Event::ArrowDown || event == Event::Character('j')) {
-            selected = (selected + 1) % (int)options.size();
+            selected_id = (selected_id + 1) % (int)options.size();
             return true;
         }
         if (event == Event::ArrowUp || event == Event::Character('k')) {
-            selected =
-                (selected - 1 + (int)options.size()) % (int)options.size();
+            selected_id =
+                (selected_id - 1 + (int)options.size()) % (int)options.size();
             return true;
         }
         return false;
@@ -47,30 +47,35 @@ std::string run_selector(
     auto screen = ScreenInteractive::TerminalOutput();
 
     screen.Loop(CatchEvent(with_keys, [&](Event event) {
-        if (cancelled) {
+        if (has_cancelled) {
             screen.ExitLoopClosure()();
             return true;
         }
-        if (confirmed) {
+        if (has_confirmed) {
             screen.ExitLoopClosure()();
             return true;
         }
 
         if (event == Event::Character('q') || event == Event::Escape) {
-            cancelled = true;
+            has_cancelled = true;
             screen.ExitLoopClosure()();
             return true;
         }
         if (event == Event::Return) {
+            has_confirmed = true;
             screen.ExitLoopClosure()();
             return true;
         }
         return false;
     }));
 
-    if (cancelled) {
+    if (has_cancelled) {
         return {};
     }
 
-    return options[selected];
+    if (!has_confirmed) {
+        return {};
+    }
+
+    return options[selected_id];
 }
